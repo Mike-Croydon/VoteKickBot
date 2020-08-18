71e9b75e5e15b0bc5b673f03bafbf5bb2c0fe9f0
Michael Croydon <mikecroydon85@gmail.com>
2020-08-17 22:02:15 -0400

    attempted to add kicking functionality, getting permissions error

--- index.js
+++ index.js
@@ -1,19 +1,76 @@
 const Discord = require('discord.js');
-const fs = require('fs');
+//const fs = require('fs');
 const client = new Discord.Client();
 
 //Token blank for commited version. Todo: add text file and read token from file, gitignore
-const token = fs.readFile('token.txt');
+const token = '';
 
+const PREFIX = '!';
+
+var version = '1.0.1'
 client.on('ready',() => {
     console.log('Bot is online');
 })
 
-client.on('message', msg=> {
-    if(msg.content === "HELLO")
-    {
-        msg.reply('Test response');
+client.on('message', message=> {
+    let args = message.content.substr(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'ping':
+            message.channel.send('pong');
+            break;
+        case 'info':
+            if(args[1] === 'version')
+            {
+                message.channel.send(version);
+            }
+            else
+            {
+                message.channel.send('Invalid args');
+            }
+            break;
+            case 'clear':
+                if(!args[1]) return message.reply('Error: please specify number of messages to delete');
+                message.channel.bulkDelete(args[1]);
+                break;
+
     }
-})
+});
+
+client.on('message', message => {
+    // Ignore messages that aren't from a guild
+    if (!message.guild) return;
+  
+    // If the message content starts with "!kick"
+    if (message.content.startsWith('!kick')) {
+      // Assuming we mention someone in the message, this will return the user
+      // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+      const user = message.mentions.users.first();
+      // If we have a user mentioned
+      if (user) {
+        // Now we get the member from the user
+        const member = message.guild.member(user);
+          member
+            .kick('Optional reason that will display in the audit logs')
+            .then(() => {
+              // We let the message author know we were able to kick the person
+              message.reply(`Successfully kicked ${user.tag}`);
+            })
+            .catch(err => {
+              // An error happened
+              // This is generally due to the bot not being able to kick the member,
+              // either due to missing permissions or role hierarchy
+              message.reply('I was unable to kick the member');
+              // Log the error
+              console.error(err);
+            });
+    // Otherwise, if no user was mentioned
+      } else {
+        message.reply("You didn't mention the user to kick!");
+      }
+    }
+  });
+
+
 
 client.login(token);
